ae5f3bcda0baf8d0f53f7c663fa1b7225b6b7de7
RalphGacayan <79454529+RalphGacayan@users.noreply.github.com>
2021-05-31 23:46:33 +0800

    Add files via upload

---
+++ Controllers/UserController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyMVC_CRUD.DataContext;
+using MyMVC_CRUD.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyMVC_CRUD.Controllers
+{
+    public class UserController : Controller
+
+    {
+        private readonly CrudDBContext _cruddbcontext;
+
+        public UserController(CrudDBContext crudDBContext)
+        {
+            _cruddbcontext = crudDBContext;
+        }
+      
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+
+        public IActionResult Register (User u)
+        {
+            if(ModelState.IsValid)
+            {
+                _cruddbcontext.Users.Add(u);
+                _cruddbcontext.SaveChanges();
+                return RedirectToAction("Allusers");
+            }
+            return View("Index");
+        }
+
+        public IActionResult Allusers()
+        {
+            return View(_cruddbcontext.Users.ToList());
+        }
+        [HttpGet]
+
+        public IActionResult Edit (int id)
+        {
+            return View(_cruddbcontext.Users.ToList());
+        }
+
+        public IActionResult Delete(int id)
+        {
+            return View();
+        }
+
+
+    }
+}
---
+++ DataContext/CrudDBContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyMVC_CRUD.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyMVC_CRUD.DataContext
+{
+    public class CrudDBContext : DbContext 
+    {
+        public CrudDBContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
